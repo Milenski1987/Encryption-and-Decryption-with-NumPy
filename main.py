bc3aa0961ff8fee480fede449cbfb13aee6c9da0
Milen Nikolov <milenski1987@icloud.com>
2025-03-15 22:35:07 +0200

    added some comments

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import numpy as np
 
 def text_to_matrix(text, size):
 #Function for converting text to matrix (ASCII codes)
-    # Преобразува всеки символ в ASCII код и ги съхранява в списък
+    # Converts each character to ASCII code and stores them in a list
     text_as_code = [ord(character) for character in text]
-    # Допълва списъка с нули, ако дължината не е кратна на размера на матрицата
+    # Appends zeros to list if the length is not a multiple of the matrix size
     while len(text_as_code) % size != 0:
         text_as_code.append(0)
-    # Преобразува списъка в NumPy матрица с размер size (брой колони)
+    # Converts the list to a NumPy matrix of size (number of columns)
     text_as_code = np.array(text_as_code)
     text_as_code = text_as_code.reshape(len(text_as_code) // 2, 2)
     return text_as_code
